6c39107621b0958d26f1d0d6a3c1b6faa822fcde
OgborFaith <faithogbor23@gmail.com>
2025-12-18 17:58:30 +0100

    Configure PWA in Next.js

---
+++ alx-movie-app/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+   images: {
+    domains: ['m.media-amazon.com'],
+      },
+
+};
+
+export default withPWA({
+  ...nextConfig
+})
+
+
